e12c2f234790039cf42be22029e6c47d5da2e9e8
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-18 20:02:08 +0530

    Handling omdb api & trigering bot event

--- bot.js
+++ bot.js
@@ -7,4 +7,23 @@ const token = '6043814170:AAGbic9sWgZ2ztmRO4bnVGyEBtOMrxeFbw4';
 const bot = new TelegramBot(token, {
     polling: true
 });
-console.log("Started")
+
+bot.on('message', async (msg) => {
+    const apiUrl = `http://www.omdbapi.com/?t=${encodeURIComponent(msg.text)}&apikey=b6ec3311`;
+        const response = await axios.get(apiUrl);
+        const movie = response.data;
+        if (movie.Response === "True") {
+            const details = [
+                `Title: ${movie.Title}`,
+                `Release Date: ${movie.Released}`,
+                `Actors: ${movie.Actors}`,
+                `Ratings: ${movie.Ratings[0].Value}`
+            ];
+            bot.sendMessage(msg.chat.id, details.join('\n'));
+        } else {
+            bot.sendMessage(msg.chat.id, "Movie not found 🎬.");
+        }
+    } 
+);
+
+console.log("started");
