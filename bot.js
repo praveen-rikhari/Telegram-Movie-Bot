01589ed58cdf4d9edec4d1e82a63ce4149e56d0a
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-18 19:57:02 +0530

    created bot and added telegram token

--- bot.js
+++ bot.js
@@ -1,4 +1,10 @@
 const axios = require('axios');
 
 const TelegramBot = require('node-telegram-bot-api');
+
+const token = '6043814170:AAGbic9sWgZ2ztmRO4bnVGyEBtOMrxeFbw4';
+
+const bot = new TelegramBot(token, {
+    polling: true
+});
 console.log("Started")
